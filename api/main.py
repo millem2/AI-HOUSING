f38a74037593bbe09905239c640f22eb060a0765
Martin Mille <martinmille@outlook.fr>
2024-02-20 11:37:38 +0100

    FEAT : first impl of api route to get similarities from image

--- api/main.py
+++ api/main.py
@@ -4,8 +4,8 @@ from typing import Union
 import sys
 
 
-from fastapi import FastAPI
-from utils import get_houses_similar_to_label
+from utils import get_houses_similar_to_label, get_housing_from_image
+from fastapi import FastAPI, File, UploadFile
 
 app = FastAPI()
 
@@ -13,6 +13,11 @@ app = FastAPI()
 @app.get("/housingFromText/")
 def get_housingFromText(label: str):
     return get_houses_similar_to_label(label)
+
+
+@app.post("/housingFromImage/")
+async def create_file(file: UploadFile = File(...)):
+    return get_housing_from_image(file)
 
 
 @app.get("/items/{item_id}")
--- api/utils.py
+++ api/utils.py
@@ -1,5 +1,10 @@
 import os
 from typing import List, Optional
+import torch
+import os
+from transformers import AutoImageProcessor, AutoModel
+from fastapi import File, UploadFile
+from PIL import Image
 import pandas as pd
 import numpy as np
 from dotenv import load_dotenv
@@ -82,3 +87,61 @@ def get_houses_similar_to_label(label: str):
 
     average = np.mean(similarities.cosine_similarity)
     print("moyenne de similarité", average)
+
+
+def get_housing_from_image(file: UploadFile = File(...)):
+
+    image1 = Image.open(file)
+    image1.show()
+
+    # Charger le modèle pré-entraîné
+    model_ckpt = "nateraw/vit-base-beans"
+    processor = AutoImageProcessor.from_pretrained(model_ckpt)
+    model = AutoModel.from_pretrained(model_ckpt)
+
+    # Charger les images (vous pouvez remplacer ces chemins par vos propres images)
+    # image_a_traiter = "../image_a_traiter/0kk3lh2o9i78bd48hor1771m10bjlxlttlkefza8w.jpg"
+    image1 = Image.open(file)
+    image1.show()
+    inputs1 = processor(images=image1, return_tensors="pt")
+    # Chemin vers les autres images à comparer
+
+    # liste des images dans le dossier "photos"
+    df = pd.read_csv("./finalDatas_embeddings.csv", index_col=0)
+
+    # Créer une nouvelle colonne pour stocker les scores de similarité
+    df["cosine_similarity"] = None
+
+    # Boucle sur chaque ligne du dataframe
+    for index, row in df.iterrows():
+
+        # On enlève les [] de la string contenue dans le fichier
+        row["photos"] = row["photos"][1:-1]
+
+        # On transform la string en une liste de liens pour pouvoir itérer sur les images
+        row["photos"] = row["photos"].split(", ")
+
+        # On va itérer sur chaque photo pour récupérer chaque score de similarité avec l'image de base. On stockera dans la ligne le meilleur score de similarité
+        for i, photo_link in enumerate(row["photos"], start=1):
+            # On charge l'image
+            similarity_scores = []
+            image = Image.open(photo_link)
+            inputs_image = processor(images=image, return_tensors="pt")
+            # On calcule les embeddings
+            with torch.no_grad():
+                embeddings1 = model(**inputs1).last_hidden_state.mean(dim=1)
+                embeddings_image = model(**inputs_image).last_hidden_state.mean(dim=1)
+            # Calculer la similarité cosinus
+            similarity_scores.push(
+                torch.nn.functional.cosine_similarity(
+                    embeddings1, embeddings_image
+                ).item()
+            )
+
+        # On va stocker dans la colonne cosine_similarity le meilleur score
+        df.at[index, "cosine_similarity"] = max(similarity_scores)
+
+    # Maintenant on sort par cosine_similarity, et on retourne le résultat trié par ordre décroissant
+    df = df.sort_values(by="cosine_similarity", ascending=False)
+
+    return df.to_json(force_ascii=False)
